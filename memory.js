5836de08193c97fb018da4b9f395d9819bd7d2cd
Kaylaesmith1 <kaylaesmith1@gmail.com>
2022-05-01 14:07:10 +0200

    Changed colors

--- memory.js
+++ memory.js
@@ -1,4 +1,49 @@
+let clickedCard = null;
+let preventClick = false;
+let combosFound = 0;
+
 function onClick(e) {
+  //can also leave it as: e.currentTarget
   const target = e.currentTarget;
+
+  if (
+    preventClick ||
+    target === clickedCard ||
+    target.className.includes("done")
+  ) {
+    return;
+  }
+
   target.className = target.className.replace("color-hidden", "");
+  target.className += "done";
+
+  if (!clickedCard) {
+    //if haven't clicked card, keep track of card and display its color
+    clickedCard = target;
+  } else if (clickedCard) {
+    //if have clicked card, see if colors match
+    if (
+      clickedCard.getAttribute("data-color") !==
+      target.getAttribute("data-color")
+    ) {
+      console.log("cards not equal");
+      preventClick = true;
+      setTimeout(() => {
+        console.log("we are here!");
+        clickedCard.className =
+          clickedCard.className.replace("done", "") + "color-hidden";
+        target.className =
+          target.className.replace("done", "") + "color-hidden";
+        clickedCard = null;
+        preventClick = false;
+      }, 500);
+    } else {
+      combosFound++;
+      clickedCard = null;
+      if (combosFound === 8) {
+        alert("YOU WIN");
+      }
+    }
+  }
 }
+
